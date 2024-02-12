1b47839f4dc6a90b311da654aefdc1d71bd6dd0c
TETIANAS09 <150555108+TETIANAS09@users.noreply.github.com>
2024-02-12 12:51:37 +0100

    Add files via upload

---
+++ file.js
@@ -0,0 +1,20 @@
+/*
+Напишіть код, щоб вибрати елемент з атрибутом data-widget-name з документа
+та прочитати його значення.
+<!DOCTYPE html>
+<html>
+<body>
+<div data-widget-name="menu">Виберіть жанр</div>
+<script>/* ваш код </script>
+</body>
+</htm>
+*/
+
+//Result//
+/*
+
+const element = document.querySelector("[data-widget-name]");
+const attributeValue = element.getAttribute("data-widget-name");
+console.log(attributeValue);
+
+*/
